cc3303be9d895dbeabf5ffd40af884a109261dbf
Intergalactic Igor <malte.baumann+bot@img.ly>
2024-08-06 17:40:50 +0000

    Update examples for v1.32.0

--- showcases-app/View/Examples/CustomDesignEditor.swift
+++ showcases-app/View/Examples/CustomDesignEditor.swift
@@ -3,7 +3,7 @@ import SwiftUI
 
 struct CustomDesignEditor: View {
   var body: some View {
-    let url = Bundle.main.url(forResource: "booklet", withExtension: "scene")!
+    let url = Bundle.main.url(forResource: "template_01_ig_post_1_1", withExtension: "scene")!
     DesignEditor(settings)
       .customEditorConfiguration(scene: url)
   }
