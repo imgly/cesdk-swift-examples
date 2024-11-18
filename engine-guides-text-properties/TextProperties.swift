40bf5746537c0c4b45d0ec2c1f4b352449ea695f
Intergalactic Igor <malte.baumann+bot@img.ly>
2024-11-18 19:09:16 +0000

    Update examples for v1.40.0-rc.0

--- engine-guides-text-properties/TextProperties.swift
+++ engine-guides-text-properties/TextProperties.swift
@@ -83,12 +83,18 @@ func textProperties(engine: Engine) async throws {
   // highlight-setFont
 
   // highlight-setTypeface
-  try engine.block.setTypeface(text, fallbackFontFileURL: typeface.fonts[3].uri, typeface: typeface)
+  try engine.block.setTypeface(text, typeface: typeface, in: "Alex".range(of: "lex")!)
+  try engine.block.setTypeface(text, typeface: typeface)
   // highlight-setTypeface
 
   // highlight-getTypeface
-  let currentTypeface = try engine.block.getTypeface(text)
+  let currentDefaultTypeface = try engine.block.getTypeface(text)
   // highlight-getTypeface
+
+  // highlight-getTypefaces
+  let currentTypefaces = try engine.block.getTypefaces(text)
+  let currentTypefacesOfRange = try engine.block.getTypefaces(text, in: "Alex".range(of: "lex")!)
+  // highlight-getTypefaces
 
   // highlight-toggleBold
   if try engine.block.canToggleBoldFont(text) {
