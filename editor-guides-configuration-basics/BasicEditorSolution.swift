a7985e7ef14288423a12c39b21176f2cade307b1
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-02-04 17:57:25 +0000

    Update examples for v1.44.0-rc.2

--- editor-guides-configuration-basics/BasicEditorSolution.swift
+++ editor-guides-configuration-basics/BasicEditorSolution.swift
@@ -8,7 +8,7 @@ struct BasicEditorSolution: View {
     // highlight-userID
     userID: "<your unique user id>",
     // highlight-baseURL
-    baseURL: URL(string: "https://cdn.img.ly/packages/imgly/cesdk-engine/1.43.0/assets")!
+    baseURL: URL(string: "https://cdn.img.ly/packages/imgly/cesdk-engine/1.44.0-rc.2/assets")!
   )
 
   var editor: some View {
